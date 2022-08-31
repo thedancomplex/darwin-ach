59e2a616893d23dc0f78938c1f72d1ff6047dbbb
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-31 18:58:02 -0400

    getting about 30 hz when just asking for FT and imu data.... this is the same as what I was getting with the ros2 driver... will have to see how much bandwidth i am using to see if that is the issue, also maybe it is a delay somewhere in the system.... I want to get abotu 100hz if possable

---
+++ tests/cpu_load_test/src/test.cpp
@@ -0,0 +1,87 @@
+/*******************************************************************************
+* Copyright 2022 Daniel M. Lofaro
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/* Author: Daniel M. Lofaro */
+
+#include "cm730.h"
+#include <unistd.h>
+
+#include <sys/time.h>
+
+double get_time()
+{
+  long seconds, useconds;
+  double duration;
+  timeval the_time;
+  gettimeofday(&the_time, NULL);
+  seconds = the_time.tv_sec;
+  useconds = the_time.tv_usec;
+
+  duration = seconds + useconds / 1000000.0;
+
+//  printf("%f\n", duration);
+  return duration;
+}
+
+
+int main()
+{
+
+
+
+
+  // Open
+  darwin::open();
+
+
+  // Turn on motor power
+  darwin::on(ID_DARWIN);
+ 
+  // Wait 1 second for power to turn on
+  usleep(1000000);
+
+  double tick = get_time();
+  double tock = get_time();
+  while(1) {
+
+    tock = get_time();
+    double T = tock - tick;
+    double f = 1/T;
+    printf("dt = %f\t f = %f\n",T,f );
+    tick = tock;
+    darwin::update_imu();
+    darwin::update_ft();
+    usleep(10000);
+  }
+
+
+  // Try to ping the Dynamixel
+  // Get Dynamixel model number
+
+  for (int i = 1; i <=20; i++)
+  {
+    darwin::ping(i);
+  }
+  darwin::ping(200);
+
+  // Turn off motor power
+  darwin::off(ID_DARWIN);
+
+  // Close port
+  darwin::close();
+
+  return 0;
+}
