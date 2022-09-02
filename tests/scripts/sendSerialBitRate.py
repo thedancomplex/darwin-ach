69174d5f2f99f18fdfeeb12c5ac27c240b059bf3
Daniel M. Lofaro <dan@danlofaro.com>
2022-09-02 12:31:28 -0400

    it seems that there may be an issue with how the serial port is implimented in the robotis sdk... will write my own and see if that helps

--- tests/scripts/sendSerialBitRate.py
+++ tests/scripts/sendSerialBitRate.py
@@ -18,9 +18,17 @@ tock = time.time()
 
 #\xff\xff\xc8\x04\x02\x03\x01-\xff\xff\xc8\x03\x00\xc8l\x00
 
-buff_tx = bytearray.fromhex("ff ff c8 04 02 03 01")
+#buff_tx = bytearray.fromhex("ff ff c8 04 02 03 01")
+buff_tx = bytearray.fromhex("ff ff c8 03 02 03 2f")
 buff_rx = bytearray.fromhex("ff ff c8 03 00 c8 00")
+
 
 while(True):
   ser.write(buff_tx)
-  time.sleep(1.0)
+  time.sleep(1/1600.0)
+  tock = time.time()
+  dt = tock - tick
+  print(dt, end='')
+  print(" f = ", end='')
+  print(1/dt)
+  tick = tock
