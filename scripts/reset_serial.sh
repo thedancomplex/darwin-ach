be03546db1509a8588ec2dfb6a99a5f60ef554bb
Daniel M. Lofaro <dan@danlofaro.com>
2022-09-07 23:34:05 -0400

    so much is not working right now, the bus is just not working

--- scripts/reset_serial.sh
+++ scripts/reset_serial.sh
@@ -14,8 +14,12 @@
 #sudo echo 1 > bConfigurationValue 
 
 
-VAL=/sys/bus/usb/devices/usb3/3-3/bConfigurationValue
+VAL=/sys/bus/usb/devices/usb2/2-2/bConfigurationValue
 sudo chmod o+w $VAL
+echo "turn off serial"
 sudo echo 0 > $VAL
+sleep 5
 
+echo "turn on serial"
 sudo echo 1 > $VAL
+sleep 1
