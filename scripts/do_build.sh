4bdf07a81827538fa1f34ca333aaf2cbbc7d0588
Daniel M. Lofaro <dan@danlofaro.com>
2022-09-12 16:08:12 -0400

    adding clock based on cpp and the ros timer

--- scripts/do_build.sh
+++ scripts/do_build.sh
@@ -1,1 +1,1 @@
-rm -rf build/darwin_lofaro/ && colcon build --symlink-install --packages-select darwin_lofaro
+rm -rf build/darwin_lofaro/ && colcon build --symlink-install --packages-select darwin_lofaro darwin_lofaro_clock
