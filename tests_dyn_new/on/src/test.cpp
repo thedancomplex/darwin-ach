411ca154b6e3053919d6b782db918968cf1fc227
Daniel M. Lofaro <dan@danlofaro.com>
2022-09-13 19:41:05 -0400

    added off test

---
+++ tests_dyn_new/on/src/test.cpp
@@ -0,0 +1,41 @@
+/*******************************************************************************
+* Copyright 2022 Daniel M. Lofaro
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/* Author: Daniel M. Lofaro */
+
+// Enable using dyn serial instead of the one Dan Lofaro made
+#define DYN_SERIAL 1
+
+// The system
+#include "lofaro_darwin.h"
+
+int main()
+{
+  // Open
+  darwin::setup("/dev/ttyUSB0");
+  //darwin::setup("/dev/ttyDARWIN1");
+
+  // Turn on motor power
+  printf("Power = %d\n", darwin::on());
+ 
+  // Wait 1 second for power to turn on
+  darwin::sleep(1.0);
+
+  // Close port
+  darwin::close();
+
+  return 0;
+}
