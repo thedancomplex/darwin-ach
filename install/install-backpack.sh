d69aded97893823b7776f81b113d8aed7e3af16f
Daniel M. Lofaro <dan@danlofaro.com>
2022-10-09 22:43:20 -0400

    added backpack install and darwin install scripts in the install dir, need to test it

---
+++ install/install-backpack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./install.sh swap
+./install.sh ros2
+./install.sh ros2-etc
+./install.sh darwin-ach base-ach
+./install.sh darwin-ach client
+./install.sh darwin-ach ros-bridge
+./install.sh darwin-auto-start-ros-bridge
---
+++ install/install-darwin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./install.sh swap-1g
+./install.sh darwin-ach base
+./install.sh darwin-ach server
+./install.sh darwin-network
+./install.sh darwin-auto-start-server
+./install.sh low-latency
